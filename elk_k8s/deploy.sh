6f9665b635a857dc04f3cc777658dc65d7ef1bc5
indicolite <indicoliteplus@gmail.com>
2017-07-17 15:58:18 +0800

    deleted unused elks

--- elk_k8s/deploy.sh
+++
@@ -1,7 +0,0 @@
-kubectl create -f es-deployment.yaml
-kubectl create -f es-service.yaml
-kubectl create -f kibana-deployment.yaml
-kubectl create -f kibana-service.yaml
-kubectl create -f logstash-config.yaml
-kubectl create -f logstash-deployment.yaml
-kubectl create -f logstash-service.yaml
