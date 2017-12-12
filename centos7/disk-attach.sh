a81097042d70bee120856d2d53344abe9565e641
indicolite <indicoliteplus@gmail.com>
2017-12-12 14:14:45 +0800

    add nas-action code branch

--- centos7/disk-attach.sh
+++ centos7/disk-attach.sh
@@ -12,6 +12,7 @@ while getopts ":i:t:s:" o; do
         t)
             t=${OPTARG}
             ((t == 'san' || t == 'nas')) || usage
+            xas=${t}
             ;;
         s)
             s=${OPTARG}
@@ -28,6 +29,13 @@ if [ -z "${i}" ] || [ -z "${t}" ] || [ -z "${s}" ]; then
     usage
 fi
 
+function nas_process(){
+    virsh  qemu-monitor-command ${num} --hmp fs ${path}
+    virsh  qemu-monitor-command ${num} --hmp device_add virtio-9p-pci,id=fs0,fsdev=fsdev-fs0,mount_tag=/shell,bus=pci.0
+}
+
+function san_process(){
+
 cat <<EOF > /tmp/disk-test.xml
 <disk type='block' device='disk'>
 <driver name='qemu' type='raw'/>
@@ -40,6 +48,17 @@ vdx=`virsh domblklist $num|awk '/dev/ {print $1}'`
 if [ -z ${vdx} ]; then
     sed -i -e "s|/dev/sdc|${path}|" /tmp/disk-test.xml
     #sed -i -e "s|vdc|${vdx}'|" /tmp/disk-test.xml
+    #virsh detach-device $num /tmp/disk-test.xml
     virsh attach-device $num /tmp/disk-test.xml
+fi
+}
+
+
+if [ ${xas} == 'san' ]; then
+    san_process
+    echo 'san process'
+elif [ ${xas} == 'nas' ]; then
+    nas_process
+    echo 'nas process'
 fi
 echo "End"
